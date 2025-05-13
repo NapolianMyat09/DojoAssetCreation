d98e8249a02d4a4d0264d73a862e4271485ca7d9
Napolian Myat <123092168+NapolianMyat09@users.noreply.github.com>
2025-05-13 15:30:11 -0400

    fix download link

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function zippedFile(){
     var link = document.createElement("a");
-    link.href = "https://github.com/NapolianMyat09/GraveyardBattlefield2.0/raw/refs/heads/main/GraveyardBattlefield2.0.zip";
-    link.download = "GraveyardBattlefield2.0.zip";
+    link.href = "https://github.com/NapolianMyat09/DojoAssetCreation/raw/refs/heads/main/DojoAssetCreationBuild.zip";
+    link.download = "DojoAssetCreationBuild.zip";
     link.click();
     document.body.removeChild(link);
 }
